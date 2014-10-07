510fbb929765c9b26174d9f7e34a7e26bc9bf7bb
bjornorri <bjornorri@gmail.com>
2014-10-07 16:50:23 +0000

    Added Parser class and unimplemented parsing methods.

---
+++ app/src/main/java/com/orangejam/ischool/Parser.java
@@ -0,0 +1,27 @@
+package com.orangejam.ischool;
+
+import com.orangejam.ischool.model.Assignment;
+import com.orangejam.ischool.model.Grade;
+
+import java.util.ArrayList;
+
+/**
+ * Created by bjornorri on 07/10/14.
+ */
+public class Parser {
+
+    public static ArrayList<Class> parseClasses(String html) {
+        ArrayList<Class> classes = new ArrayList<Class>();
+        return classes;
+    }
+
+    public static ArrayList<Assignment> parseAssignments(String html) {
+        ArrayList<Assignment> assignments = new ArrayList<Assignment>();
+        return assignments;
+    }
+
+    public static ArrayList<Grade> parseGrades(String html) {
+        ArrayList<Grade> grades = new ArrayList<Grade>();
+        return grades;
+    }
+}
