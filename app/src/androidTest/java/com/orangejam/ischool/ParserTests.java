4b83529f82e66139ff889ef1fde97956e365044a
bjornorri <bjornorri@gmail.com>
2014-10-07 22:28:43 +0000

    Changed encoding and method of reading file in unit tests.

--- app/src/androidTest/java/com/orangejam/ischool/ParserTests.java
+++ app/src/androidTest/java/com/orangejam/ischool/ParserTests.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import org.jsoup.nodes.Document;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -34,10 +35,11 @@ public class ParserTests extends ApplicationTestCase<Application> {
         try {
             InputStream inputStream = getContext().getAssets().open(pageName);
             StringBuilder stringBuilder =  new StringBuilder();
-            BufferedReader in = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-            String str;
-            while ((str = in.readLine()) != null) {
-                stringBuilder.append(str);
+            BufferedReader in = new BufferedReader(new InputStreamReader(inputStream, "ISO-8859-1"));
+            int aByte;
+            while ((aByte = in.read()) != -1) {
+                char c = (char)aByte;
+                stringBuilder.append(c);
             }
             inputStream.close();
             page = stringBuilder.toString();
