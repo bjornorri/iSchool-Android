10f26131df10a57772a30d6ea2219613c7206702
bjornorri <bjornorri@gmail.com>
2014-10-07 18:14:58 +0000

    Implemented equals() and hashCode() for Class class.

--- app/src/main/java/com/orangejam/ischool/model/Class.java
+++ app/src/main/java/com/orangejam/ischool/model/Class.java
@@ -39,4 +39,49 @@ public class Class {
         Calendar now = new GregorianCalendar();
         return (now.after(this.startDate) && now.before(this.endDate));
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if(o == this) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        if(!o.getClass().equals(Class.class)) {
+            return false;
+        }
+        Class otherClass = (Class)o;
+        if(!otherClass.courseName.equals(this.courseName)) {
+            return false;
+        }
+        if(!otherClass.type.equals(this.type)) {
+            return false;
+        }
+        if(!otherClass.location.equals(this.location)) {
+            return false;
+        }
+        if(!otherClass.startDate.equals(this.startDate)) {
+            return false;
+        }
+        if(!otherClass.endDate.equals(this.endDate)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        for(int i = 0; i < courseName.length(); i++) {
+            hash += (int)courseName.charAt(i);
+        }
+        for(int i = 0; i < type.length(); i++) {
+            hash += (int)type.charAt(i);
+        }
+        for(int i = 0; i < location.length(); i++) {
+            hash += (int)location.charAt(i);
+        }
+        return hash;
+    }
 }
