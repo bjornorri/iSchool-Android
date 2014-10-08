8b2ec5cf74cf7648e9ebc93ba1ef0593562eab33
bjornorri <bjornorri@gmail.com>
2014-10-08 17:55:22 +0000

    Added the option to fetch a page on the current thread.

--- app/src/main/java/com/orangejam/ischool/NetworkClient.java
+++ app/src/main/java/com/orangejam/ischool/NetworkClient.java
@@ -30,7 +30,7 @@ public class NetworkClient {
 
     /* Fetches a HTML page from a given URL asynchronously.
      * The caller must provide a success handler and an error handler that get called on success and error. */
-    public static void fetchPage(final Context context, final String pageURL, final Handler successHandler, final Handler errorHandler) {
+    public static void fetchPageAsynchronous(final Context context, final String pageURL, final Handler successHandler, final Handler errorHandler) {
 
         new Thread(new Runnable() {
             @Override
@@ -76,5 +76,46 @@ public class NetworkClient {
                 }
             }
         }).start();
+    }
+
+    /* Fetches a HTML page from a given URL.
+     * It is the callers responsibility to do this asynchronously. */
+    public static String fetchPage(Context context, String pageURL) {
+        String html = null;
+        // Fetch the stored credentials.
+        String username = CredentialManager.getUsername(context);
+        String password = CredentialManager.getPassword(context);
+        // Don't do anything if the credentials are missing.
+        if(username == null || password == null) {
+            return null;
+        }
+        // Create a CredentialsProvider with the stored credentials.
+        CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT),
+                new UsernamePasswordCredentials(username, password));
+        // Create the HTTP client.
+        DefaultHttpClient httpClient = new DefaultHttpClient();
+        httpClient.setCredentialsProvider(credentialsProvider);
+        // Craft the request.
+        HttpGet getRequest = new HttpGet(pageURL);
+        try {
+            // Send the request and get the response.
+            HttpResponse response = httpClient.execute(getRequest);
+            int statusCode = response.getStatusLine().getStatusCode();
+            Log.d("NetworkClient", "Status Code: " + statusCode);
+            // If the status code is 200 the request was successful.
+            if(statusCode == 200) {
+                html = EntityUtils.toString(response.getEntity());
+            }
+            // If the request was not successful send the error handler a message containing the status code.
+            else {
+                html = null;
+            }
+        } catch(ClientProtocolException e) {
+            Log.d("NetworkClient", "Client protocol exception", e);
+        } catch(IOException e) {
+            Log.d("NetworkClient", "IOException", e);
+        }
+        return html;
     }
 }
