dc99650a76c50b2f1313c1578f7a353798dc457e
bjornorri <bjornorri@gmail.com>
2014-10-08 04:10:20 +0000

    Made small changes to Grade model class.

--- app/src/main/java/com/orangejam/ischool/model/Grade.java
+++ app/src/main/java/com/orangejam/ischool/model/Grade.java
@@ -8,12 +8,12 @@ public class Grade {
     public String assignmentName;
     public String courseName;
     public Float grade;
-    public int firstRank;
-    public int lastRank;
+    public Integer firstRank;
+    public Integer lastRank;
     public String feedback;
     public String URL;
 
-    public Grade(String assignmentName, String courseName, Float grade, int firstRank, int lastRank, String feedback, String URL) {
+    public Grade(String assignmentName, String courseName, Float grade, Integer firstRank, Integer lastRank, String feedback, String URL) {
         this.assignmentName = assignmentName;
         this.courseName = courseName;
         this.grade = grade;
