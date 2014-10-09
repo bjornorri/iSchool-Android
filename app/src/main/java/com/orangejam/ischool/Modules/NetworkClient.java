f6f44aa518450b0fa63243a081f0088be8d985b8
bjornorri <bjornorri@gmail.com>
2014-10-09 18:03:30 +0000

    Removed asynchronous fetching method from NetworkClient.

--- app/src/main/java/com/orangejam/ischool/Modules/NetworkClient.java
+++ app/src/main/java/com/orangejam/ischool/Modules/NetworkClient.java
@@ -24,56 +24,6 @@ import java.io.IOException;
 
 public class NetworkClient {
 
-    /* Fetches a HTML page from a given URL asynchronously.
-     * The caller must provide a success handler and an error handler that get called on success and error. */
-    public static void fetchPageAsynchronous(final Context context, final String pageURL, final Handler successHandler, final Handler errorHandler) {
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                // Fetch the stored credentials.
-                String username = CredentialManager.getUsername(context);
-                String password = CredentialManager.getPassword(context);
-                // Don't do anything if the credentials are missing.
-                if(username == null || password == null) {
-                    return;
-                }
-                // Create a CredentialsProvider with the stored credentials.
-                CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-                credentialsProvider.setCredentials(new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT),
-                        new UsernamePasswordCredentials(username, password));
-                // Create the HTTP client.
-                DefaultHttpClient httpClient = new DefaultHttpClient();
-                httpClient.setCredentialsProvider(credentialsProvider);
-                // Craft the request.
-                HttpGet getRequest = new HttpGet(pageURL);
-                try {
-                    // Send the request and get the response.
-                    HttpResponse response = httpClient.execute(getRequest);
-                    int statusCode = response.getStatusLine().getStatusCode();
-                    Log.d("NetworkClient", "Status Code: " + statusCode);
-                    // If the status code is 200 the request was successful.
-                    if(statusCode == 200) {
-                        String html = EntityUtils.toString(response.getEntity());
-                        Message message = Message.obtain();
-                        message.obj = html;
-                        successHandler.sendMessage(message);
-                    }
-                    // If the request was not successful send the error handler a message containing the status code.
-                    else {
-                        errorHandler.sendEmptyMessage(statusCode);
-                    }
-                } catch(ClientProtocolException e) {
-                    errorHandler.sendEmptyMessage(-1);
-                    Log.d("NetworkClient", "Client protocol exception", e);
-                } catch(IOException e) {
-                    errorHandler.sendEmptyMessage(-1);
-                    Log.d("NetworkClient", "IOException", e);
-                }
-            }
-        }).start();
-    }
-
     /* Fetches a HTML page from a given URL.
      * It is the callers responsibility to do this asynchronously. */
     public static String fetchPage(Context context, String pageURL) {
