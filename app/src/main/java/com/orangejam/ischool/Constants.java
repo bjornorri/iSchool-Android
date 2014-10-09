57bb7d0bb13b6f0ebed9a1e193cfacbb619b0e0b
bjornorri <bjornorri@gmail.com>
2014-10-09 13:01:30 +0000

    Created Constants class.

---
+++ app/src/main/java/com/orangejam/ischool/Constants.java
@@ -0,0 +1,19 @@
+package com.orangejam.ischool;
+
+/**
+ * Created by bjornorri on 09/10/14.
+ */
+public class Constants {
+
+    // MySchool page URLs.
+    public static final String AssignmentsURL = "https://myschool.ru.is/myschool/?Page=Exe&ID=1.12";
+    public static final String TimetableURL = "https://myschool.ru.is/myschool/?Page=Exe&ID=3.2";
+
+    // The page URL for Málið.
+    public static final String CanteenURL = "http://malid.ru.is";
+
+    // Intent actions.
+    public static final String TimetableNotification = "DataStoreDidFinishLoadingClassesNotification";
+    public static final String AssignmentsNotification = "DataStoreDidFinishLoadingAssignmentsNotification";
+    public static final String GradesNotification = "DataStoreDidFinishLoadingGradesNotification";
+}
