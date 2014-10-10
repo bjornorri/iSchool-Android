0f5dae4bacc1297701f6687e87a88e33daba8f0e
bjornorri <bjornorri@gmail.com>
2014-10-10 01:16:24 +0000

    Added cool SwipeRefreshLayout to the timetable fragment.

--- app/src/main/java/com/orangejam/ischool/fragments/TimetableFragment.java
+++ app/src/main/java/com/orangejam/ischool/fragments/TimetableFragment.java
@@ -1,19 +1,37 @@
 package com.orangejam.ischool.fragments;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.ListFragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.orangejam.ischool.R;
+import com.orangejam.ischool.modules.DataStore;
 
-public class TimetableFragment extends Fragment {
+public class TimetableFragment extends ListFragment {
+
+    private SwipeRefreshLayout mSwipeLayout;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_timetable, container, false);
+        mSwipeLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.swipe_container);
+        mSwipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.d("TimetableFragment", "Refreshing");
+                Log.d("", "isRefreshing(): " + mSwipeLayout.isRefreshing());
+                DataStore.getInstance(getActivity().getApplicationContext()).fetchClasses();
+            }
+        });
+        mSwipeLayout.setColorSchemeResources(android.R.color.holo_red_light,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_blue_bright);
         return rootView;
     }
 }
